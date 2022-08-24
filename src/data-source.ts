f0b19b3e424ea12c13674374ab1ce7dd02a30681
Thiago <thiago.dias.148116@gmail.com>
2022-08-24 18:46:04 -0300

    fix: fixing heroku bug

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,6 +8,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DB_URI_DEV,
+  ssl: {rejectUnauthorized: false},
   logging: false,
   entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
   migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
